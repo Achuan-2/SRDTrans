296fd720bf370015c83568e67f6e27e5f718f88c
Tron <achuan-2@outlook.com>
2026-09-16 16:49:04 +0800

    🎨 添加test降噪脚本

--- test.sh
+++ test.sh
@@ -1,7 +1,10 @@
+#!/usr/bin/env bash
+set -e
+# Run from a terminal with the srdtrans environment already activated.
 python test.py \
   --datasets_path datasets \
-  --datasets_folder my_data \
-  --denoise_model cad_03hz \
+  --datasets_folder PFC \
+  --denoise_model PFC_202609160005 \
   --GPU 0 \
   --patch_x 64 \
   --patch_t 160
